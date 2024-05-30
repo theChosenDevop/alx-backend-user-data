4a3a27843ffe84c182e74212daef7e874926a99a
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-30 20:10:22 +0000

    update

--- 0x00-personal_data/filtered_logger_copy.py
+++ 0x00-personal_data/filtered_logger_copy.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 """filtered_logger module"""
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] % (name)s % (levelname)s % (asctime) - 15s:
+        %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
 
 
 def filter_datum(fields: list, redaction: str, message: str,
