174a1f301fc31af73c1930557f747260bb9b7886
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-01 20:11:51 +0000

    update 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,14 +29,14 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class"""
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    REDACTION: str = "***"
+    FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR: str = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: List[str]) -> None:
         """Initialize Object"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+        self.fields: List[str] = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """returns a formatted log
