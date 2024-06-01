1448e90e3c0345574f86f24422a7e56b73ba581e
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-01 19:42:58 +0000

    test3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,10 @@ import re
 import logging
 import mysql.connector
 import os
+from typing import List, Tuple
 
 
-PII_FIELDS = ('name', 'email', 'phonenumber', 'ssn', 'password')
+PII_FIELDS: Tuple[str] = ('name', 'email', 'phonenumber', 'ssn', 'password')
 
 
 def filter_datum(fields: list, redaction: str, message: str,
@@ -32,7 +33,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: tuple):
+    def __init__(self, fields: List[str]):
         """Initialize Object"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
