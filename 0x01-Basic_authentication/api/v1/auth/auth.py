f961499b12b6352328bfed5635a6706aab41ff5a
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-05 00:26:57 +0000

    auth verification

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,10 +11,26 @@ class Auth:
         """ïnitialaization of object"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
+        """checks if a given path requires authentication
+        Args:
+          path: The path to check
+          excluded_paths: A list of paths that do not require
+                          authentication
         Returns:
-          - False False - path
+          - False if does not require authentication or otherwise True
         """
+        if path is None or excluded_paths is None:
+            return True
+        # ensure path has a trailing slash
+        if not path.endswith('/'):
+            path += '/'
+        # ensure excluded_paths have a slash
+        excluded_paths = [
+                route_path if route_path.endswith('/')
+                else route_path + '/' for route_path in excluded_paths
+                ]
+        if path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
