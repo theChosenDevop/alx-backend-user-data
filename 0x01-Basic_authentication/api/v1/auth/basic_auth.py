32d0539ab8134d2b2f71a9e004bbe9a3bbbf0869
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-05 11:11:23 +0000

    decode base64 objects

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,7 +12,7 @@ class BasicAuth(Auth):
             self, authorization_header: str) -> str:
         """Extracts base64 authorization header
         Args:
-            authorization_header [str]: Base64 string
+            authorization_header [str]: string input
         Returns:
           - Base64 part of the Authorization header for a Basic Authentication
          """
@@ -21,12 +21,31 @@ class BasicAuth(Auth):
         # check if the input is a string with isinstance(input, str)
         if not isinstance(authorization_header, str):
             return None
-        # slicing the 4 characters of Basic from the string
+        # split the string characters of Basic by space
         auth_var = authorization_header.rsplit(" ")
         if auth_var[0] != "Basic":
             return None
         else:
             return auth_var[1]
-        # assign the above to a variable
-        # compare if slice equals Basic
-        # return the remaining of the slice
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decodes base64 string
+        Args:
+            base64_authorization_header [str]: base64 string
+        Returns:
+            - decoded value of Base64 string
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            # convert it to bytes object
+            string_object = base64_authorization_header.encode('ascii')
+            # decode it to base64 bytes
+            base64_object = base64.b64decode(string_object)
+            # decode it to utf string
+            return base64_object.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
