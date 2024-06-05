0c357320f3c74c0418a8175a83538732b7c1b87a
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-05 09:30:52 +0000

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """BasicAuth module"""
-from ap.v1.auth.auth import Auth
+import base64
+from api.v1.auth.auth import Auth
 
 
-class BasicAuth:
+class BasicAuth(Auth):
     """Inherits from Auth"""
     pass
