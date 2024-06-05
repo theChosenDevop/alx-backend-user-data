153caa6c11813d0064e2f15666e17bef531bc5e5
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-05 04:59:17 +0000

    environment comparison

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,7 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,9 +14,13 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 auth_inst = getenv("AUTH_TYPE")
-if auth_inst == "auth":
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+if auth_inst:
+    if auth_inst == "auth":
+        from api.v1.auth.auth import Auth
+        auth = Auth()
+    elif auth_inst == "basic_auth":
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -48,7 +52,7 @@ def route_before_request() -> str:
     excluded_path = [
             '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
             ]
-    if auth.require_auth(request.path, excluded_path):
+    if not auth.require_auth(request.path, excluded_path):
         return
     if auth.authorization_header(request) is None:
         raise abort(401)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""BasicAuth module"""
+from ap.v1.auth.auth import Auth
+
+
+class BasicAuth:
+    """Inherits from Auth"""
+    pass
