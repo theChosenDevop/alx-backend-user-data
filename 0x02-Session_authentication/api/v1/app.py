0168b11c879d4d666276987e75d9e431edcd4f78
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-06 20:35:36 +0000

    basic auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,7 +38,7 @@ def before_request() -> str:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    request.current_user = Auth().current_user(request)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -6,6 +6,16 @@ from flask import abort, jsonify, request
 from models.user import User
 
 
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def get_me() -> str:
+    """GET /api/v1/users/me
+    retrieve the authenticated User object.
+    """
+    if request.current_user is None:
+        raise abort(404)
+    return jsonify(request.current_user.to_json())
+
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """GET /api/v1/users
@@ -30,8 +40,11 @@ def view_one_user(user_id: str = None) -> str:
     user = User.get(user_id)
     if user is None:
         abort(404)
+    if user_id == 'me' and request.current_user is None:
+        abort(404)
+    if user_id == 'me' and request.current_user:
+        return jsonify(user.to_json())
     return jsonify(user.to_json())
-
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
