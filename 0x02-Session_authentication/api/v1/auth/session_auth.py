8790a2b3b167bc3198cc797b0e2faed1cf680486
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-06 21:25:28 +0000

    update session aut

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,6 +13,8 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """Creates a Session ID for a user_id
+        Args:
+            user_id [str]: user identification
         Returns:
             None if user_id is None or not a string
         """
@@ -24,7 +26,13 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """"""
+        """returns a User ID based on a Session ID
+        Args:
+            user_id [str]: user identification
+        Returns:
+            the value (the User ID) for the key session_id in the
+            dictionary user_id_by_session_id.
+        """
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
