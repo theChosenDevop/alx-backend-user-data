20ae4365b29d333c6578610256433e5d5c5f302b
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-06 02:36:46 +0000

    basic auth update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,7 +8,6 @@ from models.user import User
 
 class BasicAuth(Auth):
     """Inherits from Auth"""
-    pass
 
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
@@ -67,7 +66,7 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         # split decoded_base64_authorization_header into index
-        email, pwd = decoded_base64_authorization_header.split(":")
+        email, pwd = decoded_base64_authorization_header.split(":", 1)
         return email, pwd
 
     def user_object_from_credentials(
@@ -89,3 +88,34 @@ class BasicAuth(Auth):
                 if user.is_valid_password(user_pwd):
                     return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns User details
+        Args:
+            - request [None]: input
+        """
+        if request is None:
+            return None
+
+        header = self.authorization_header(request)
+        if header is None:
+            return None
+
+        header = self.authorization_header(request)
+        if header is None:
+            return None
+        extract = self.extract_base64_authorization_header(header)
+        if extract is None:
+            return None
+        decode = self.decode_base64_authorization_header(extract)
+        if decode is None:
+            return None
+        email, pwd = self.extract_user_credentials(decode)
+        if email is None or pwd is None:
+            return None
+
+        user = self.user_object_from_credentials(email, pwd)
+        if user is None:
+            return None
+
+        return user
