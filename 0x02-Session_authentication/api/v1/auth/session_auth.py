f25226e0f3ba77409b9148f32d1d5ea9085cfc26
theChosenDevop <tobbey.adesanya@gmail.com>
2024-06-06 21:10:04 +0000

    session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 """SessionAuth Module"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Inherits from superclass Auth"""
 
+    def __init__(self):
+        """initialization of class"""
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id
+        Returns:
+            None if user_id is None or not a string
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
