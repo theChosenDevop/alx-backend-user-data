a2b290de23c8025adb66462d6208b0f3a4b1a5ea
theChosenDevop <tobbey,adesanya@gmail.com>
2024-06-11 15:14:12 +0000

    raise exceptions

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
