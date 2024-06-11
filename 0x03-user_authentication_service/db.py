3930d6d11f3f11da4e09e0124e324ea78206820f
theChosenDevop <tobbey,adesanya@gmail.com>
2024-06-11 12:52:02 +0000

    update task

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,10 +1,12 @@
 """DB module
 """
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from user import User, Base
+
+from user import Base, User
 
 
 class DB:
@@ -14,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -40,4 +42,5 @@ class DB:
         session = self._session
         session.add(new_user)
         session.commit()
+
         return new_user
