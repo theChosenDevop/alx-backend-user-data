9bd50281912f6a3776303b70c543459afd5fdfc9
theChosenDevop <tobbey,adesanya@gmail.com>
2024-06-12 07:42:43 +0000

    hash passwors

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 """Auth Module
 """
+import bcrypt
 
+gen_salt = bcrypt.gensalt(rounds=12)
+
+
+def _hash_password(password: str) -> bytes:
+    """hash user password
+      Args
+        password [str]: user input
+      Returns:
+          bytes
+    """
+    pwd = password.encode('utf-8')
+    return bcrypt.hashpw(pwd, gen_salt)
