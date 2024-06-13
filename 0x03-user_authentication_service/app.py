3cd220e8bcddc858cbe08d517b70aaf7e0fa26cb
theChosenDevop <tobbey,adesanya@gmail.com>
2024-06-13 12:07:25 +0000

    update reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,7 +82,7 @@ def get_reset_password_token():
         abort(400, description="Email required")
     try:
         reset_token = AUTH.get_reset_password_token(email)
-        jsonify({"email": email, "reset_token": reset_token}), 200
+        return jsonify({"email": email, "reset_token": reset_token}), 200
     except ValueError:
         abort(403)
 
